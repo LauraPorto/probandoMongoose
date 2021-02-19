458638f4e2ad0ae242cb5a87580e5cb0d0ea4ec8
Laura <lauraporto596@gmail.com>
2021-02-19 10:40:12 +0100

    learn env vars

---
+++ learnEnv.js
@@ -0,0 +1,6 @@
+console.log(process.env.MI_NOMBRE);
+
+
+/* Mongo tiene una serie de variables, con métodos entre losq ue están process, que es un objeto que es process, que tiene a su vez un método que es env y que contiene información. En process.env se guardan las VARIABLES DE ENTORNO.
+Las variables de entorno se escriben con mayúscula y con guiones bajos si hay más de una palabra 
+El contenido de las variables no se escriben en código, sino en terminal (esa es la buena práctica)*/
