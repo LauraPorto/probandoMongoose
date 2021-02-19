8a05c82ac4df7671ff78e5adda8314acb2eebe13
Laura <lauraporto596@gmail.com>
2021-02-19 13:45:45 +0100

    add notes

--- writeExamples.js
+++ writeExamples.js
@@ -81,31 +81,16 @@ kitty = 'Rudolf'; //Cambiamos valor de la propiedad required antes de guardarla
 logger('Guardando en la base de datos la instancia');
 kitty.save().then(() => logger('instancia de cat salvada')).catch((error) => console.log(error));
 
-/* Definimos un modelo al que le daremos propiedades (de un modo similar a las propiedades de la clase botón en la calculadora). Se crea un objeto de propiedades sin constructor en las que se tipa las propiedades (string, numero...). Este objeto se realiza mediante un Schema, una función de mongoose, como una instancia de clase que aplica las normas o propiedades de mongoose y es lo que tenemos que meter como segundo parámetro al método model. 
+/* 
+Definimos un modelo al que le daremos propiedades (de un modo similar a las propiedades de la clase botón en la calculadora). Se crea un objeto de propiedades sin constructor en las que se tipa las propiedades (string, numero...). Este objeto se realiza mediante un Schema, una función de mongoose, como una instancia de clase que aplica las normas o propiedades de mongoose y es lo que tenemos que meter como segundo parámetro al método model. 
 Nos devuelve una instancia del objeto tipo Schema en el que definiremos cómo son los datos. Podemos construir objetos y clases dentro del objeto schema, esto incluye el required=true, lo cual hace de una propiedad un requerimiento haciendo indispensable que introduzcas X tipos de datos (cubrir datos obligatorios para poder instanciar, por ejemplo, el caso de un email para darse de alta en una página, llevando a cabo ese control el modelo y no nosotros mismos). De modo similar, con el default podemos indicar que se autorellenen datos que no han sido completados a nuestro criterio (default y required, aunque mongoose se lo traga, conceptualmente no son compatibles). Es común aplicar valores default, por ejemplo, a la fecha de creación de usuario o pedido... Aplicado al proyecto de las películas, cada esquema correspondería, por ejemplo, a pedidos, películas y usuarios. 
 OBJECT ID: esta objeto de propiedad nos genera un ID para cada proceso no correlacional y no hackeable, como un código hash, que sería una instancia del objeto ID cada vez que lo genera. 
 Se puede meter datos booleanos para optimizar la clasificación de los datos (ej. gatos esterilizados).
 
-Este modelo se puede aplicar a Cat y a Dog, y se puede hacer nuevas instancias a partir de este objeto modelo que tendrán las propiedades definidas y métodos. Uno de los métodos es save, que guarda la instancia en la base de datos. Para instanciar sólamente será obligatorio los datos que sean required, y se pueden añadir más adelante más propiedades o rellenar las que no habíamos cubierto [Esquema - modelo - instancia]
+Este modelo se puede aplicar a Cat y a Dog, y se puede hacer nuevas instancias a partir de este objeto modelo que tendrán las propiedades definidas y métodos. Uno de los métodos es save, que guarda la instancia en la base de datos. Para instanciar sólamente será obligatorio los datos que sean required, y se pueden añadir más adelante más propiedades o rellenar las que no habíamos cubierto [Esquema - modelo - instancia].
+Hay diferentes métodos para el modelo y para la instancia (ej. find tiene sentido aplicarlo al modelo, no a una instancia de un modelo que sería un gato solo).
 
 SAVE() nos devuelve una promesa (por proceso interno de mongoose) de que la instancia se guardará una vez se haya establecido la conexión. Para conocer esto, encadenamos con una promesa hecha por nosotros en esta ocasión, un .then, para que nos avise de cuando se ha salvado la instancia y, por tanto, de cuando la conexión se ha establecido. 
 
 EN BACKEND ES OBLIGATORIO HACER EL MANEJO DE ERROR.
-*/
-
-
-
-
-/*
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', funcion() {
-    console.log("We are connected !")
-});
-
-const kittySchema = new mongoose.Schema({
-    name: String
-});
-
-const Kitten = mongoose.model('Kitten', kittySchema);
 */
